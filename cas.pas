6f7a4790dd54e28e21513e4d77e9fd21c369fe54
Nahuel <nahuelarancibia4@gmail.com>
2022-08-30 19:45:39 -0300

    TERCER COMIT

--- cas.pas
+++ cas.pas
@@ -2,4 +2,15 @@
 Program la;
 Begin
   Writeln ('lacaaeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaa');
+  Procedure IMPRIMIR NOMBRES
+  Begin
+    Writeln ('Nombres');
+  End;
+  ObjectName := TObject.Create(ObjectName);
+  Try
+    TObject
+  Finally
+    ObjectName.Free;
+End;
 End.
+CASAAAAAAA
